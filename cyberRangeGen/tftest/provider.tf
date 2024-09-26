8b603ed30468477f6f6286bfb0de03136642c918
AdryGiuliani <giulianiadriano3@gmail.com>
2024-09-26 15:09:34 +0200

    Add provider.tf

---
+++ cyberRangeGen/tftest/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc1"
+    }
+  }
+}
+
+
+provider "proxmox" {
+  //your api credentials
+  pm_api_url      = ""
+  pm_api_token_id = ""
+  pm_api_token_secret = ""
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
